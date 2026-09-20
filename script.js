934e0f8d34bc84cf3bc4649ccb9f336b170080af
mprasadilal0-dotcom <mprasadilal0@gmail.com>
2026-09-20 15:17:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 
-// Rehan Ludo 🎲
 
-let diceNumber = 1;
-
-function rollDice() {
-  diceNumber = Math.floor(Math.random() * 6) + 1;
-
-  const dice = document.getElementById("dice");
-
-  if (dice) {
-    dice.innerText = diceNumber;
-  }
-
-  console.log("Dice:", diceNumber);
-}
-
-console.log("Rehan Ludo Ready!");
